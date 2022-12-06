abef00460a26458dcdbcc8cc4dfc47f18859a9f5
tacicek <tuncaycicek@outlook.com>
2022-12-06 03:40:42 +0100

    create pages and configration links

---
+++ pages/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
